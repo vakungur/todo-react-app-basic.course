90f1ad66a6017c977e434db3b63f6cc0b0a32638
vakungur <vakungur@gmail.com>
2020-06-07 00:47:38 +0300

    complete structure project

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-panel';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <AppHeader />
+      <div className="search-panel d-flex">
+        <SearchPanel />
+        <PostStatusFilter />
+      </div>
+      <PostList />
+      <PostAddForm />
+    </div>
+  );
+};
+
+export default App;
